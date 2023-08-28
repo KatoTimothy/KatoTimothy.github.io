cee5891eb278ecc6322a3730196dfcfcb6326ce9
unknown <katotimuci@gmail.com>
2023-08-28 15:09:02 +0300

    Added a swiping functionality to the membership section

--- js/swiper.js
+++ js/swiper.js
@@ -1,2 +1,22 @@
-import Swiper from 'swiper'
-import {} from 'swiper/modules';                   
+//imports core modules
+import Swiper from "swiper";
+import { Pagination, Navigation } from "swiper/modules";
+
+(function () {
+  "use strict";
+  const membershipSwiperContainerEl = document.querySelector(
+    ".membership-cards__swiper"
+  );
+  const membershipCardsEl = document.querySelector("#membership-cards");
+
+  membershipSwiper = new Swiper(membershipSwiperContainerEl, {
+    modules: [Navigation, Pagination],
+    spaceBetween: 12,
+    slidesPerView: "auto",
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+})();
